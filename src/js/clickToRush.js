6a29ad1bd626dd342a95227ed1a866819f48113d
Alan Harnum <waharnum@gmail.com>
2017-01-29 18:04:21 -0500

    Quotes, text outline.

--- src/js/clickToRush.js
+++ src/js/clickToRush.js
@@ -11,7 +11,19 @@ $(document).ready(function () {
         "You, the university, the clouds in the sky, all just a dream, a distant memory, and one I'd really rather went away.",
         "Ascension. It's a process whereby consciousness converts to energy and no longer requires a physical form.",
         "But I can tell you that fear, well, it's just one of those things in this world that proves beyond a shadow of a doubt that you're truly alive.",
-        "For a moment there I thought we were in trouble"
+        "For a moment there I thought we were in trouble",
+        "You think just because you were born poor, that gives you the right to be angry at the world! How pathetic!",
+        "My father worked in the shipyards in Glasgow! I earned a scholarship to Oxford while I was working two jobs! I have earned the right to make decisions without explaining myself to you or anyone else!",
+        "I refuse to be held responsible for this situation!",
+        "Solving the issues of dialling the ninth chevron. I'll devote two and a half years of my life to that. Meanwhile, my wife is gonna spend her dying days alone while I'm off, out, trying to solve that little problem.",
+        "I always had it in me, to make the hard decisions. I have reasons. Good reasons.",
+        "I warn you, most people find these rather bitter.",
+        "Are you familiar with the cosmic microwave background radiation?",
+        "We're talking about a mystery rooted in the foundation of reality, a puzzle with pieces scattered across the length and breadth of the universe itself.",
+        "You know we grew up taught to believe that everyone's equal. That you are no better than anyone else – Of course that's a lie. Some are better than others. And there are those who recognize what makes them better and are able to exploit that to succeed.",
+        "See what you can do when you apply yourself?",
+        "Oh, bloody hell! We're all doomed!",
+        "Well, we can't run, we can't hide. We'll have to fight."
     ];
 
     var getRandomInt = function(min, max) {
@@ -64,7 +76,5 @@ $(document).ready(function () {
     rush.click(function () {
         restartAnimation();
     });
-
-    restartAnimation();
 
 });
